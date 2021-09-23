76dba9d64b91d758d2f3fef8b9078db561782cf4
voidzk <zkadry97@gmail.com>
2021-09-23 04:46:54 +0200

    removing factor

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+
 import View from './View.js';
 
 class RecipeView extends View {
@@ -107,9 +107,7 @@ class RecipeView extends View {
       <svg class="recipe__icon">
       <use href="${icons}#icon-check"></use>
       </svg>
-      <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
-      }</div>
+      <div class="recipe__quantity">${ing.quantity}</div>
       <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
       ${ing.description}
