8bcfef4cf6087608ff079cdc8dea9f795e245a2a
Ziad <91224200+voidzk@users.noreply.github.com>
2022-03-07 02:02:43 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '5ebebcb3-c740-4d3f-ada9-d9d2ff9345de';
+
 export const MODEL_CLOSE_SEC = 2.5;
